2c25d7416126d2a70aa66783ddf1aeaeaa34582f
KingZTzy <kenazsambur@gmail.com>
2023-02-07 22:34:24 +0700

    fix(login): stringify user object on localstorage

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,7 +4,7 @@ import { useRoute, useRouter } from 'vue-router';
 import { useAuthRepository } from '@/composables';
 
 const repository = useAuthRepository()
-const route = useRoute() 
+const route = useRoute()
 const router = useRouter()
 
 const credenstials = reactive({
@@ -18,13 +18,13 @@ const onSubmit = async () => {
   isLoggingIn.value = true
 
   try {
-    const {data} = await repository.login(credenstials)
+    const { data } = await repository.login(credenstials)
     if (data) {
       localStorage.setItem('access_token', data.access_token)
-      localStorage.setItem('user', data.user)
-      router.replace({name: "about"})
-    } 
-  } catch(e) {
+      localStorage.setItem("user", JSON.stringify(data.user));
+      router.replace({ name: "restos" })
+    }
+  } catch (e) {
     console.error(e)
   }
 
@@ -33,9 +33,10 @@ const onSubmit = async () => {
 </script>
 
 <template>
-  <main class="grid grid-cols-12 gap-4 min-h-screen">
+  <main class="grid grid-cols-12 min-h-screen">
     <section class="col-span-6 bg-white h-full shadow-xl">
-      <form :action="route.path" method="post" class="p-40" @submit.prevent="onSubmit">
+      <form :action="route.path" method="post" class="p-40 font-light" @submit.prevent="onSubmit">
+        <h1 class="block text-center mb-2 text-3xl">Login</h1>
 
         <div class="mb-4">
           <label for="email" class="block mb-2">Email</label>
@@ -48,8 +49,10 @@ const onSubmit = async () => {
           <input v-model="credenstials.password" type="password" required
             class="border p-2 w-full bg-gray-50 outline-none focus:ring-4 focus:ring-blue-300 rounded">
         </div>
-
-        <button type="submit" class="bg-blue-600 text-white p-2 w-full block hover:bg-blue-800 rounded transition-colors duration-200">Masuk</button>
+        <small class="block mb-2">Don't Have Account? <router-link to="#" class="text-blue-600">Register
+            Here!</router-link> </small>
+        <button type="submit"
+          class="bg-blue-600 text-white p-2 w-full block hover:bg-blue-800 rounded transition-colors duration-200">Masuk</button>
       </form>
     </section>
   </main>
